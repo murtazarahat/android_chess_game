e75fdfefa7921bc9213a963256b363d625f3d457
Syed Rahat Murtaza <rahat_murtaza@outlook.com>
2022-01-29 01:07:21 +0500

    Code Refactored

---
+++ app/src/main/java/com/example/chessappliation/Position.java
@@ -0,0 +1,22 @@
+package com.example.chessappliation;
+
+import com.example.chessappliation.Pieces.Piece;
+
+public class Position {
+    private Piece piece;
+
+
+    Position(Piece piece ) {
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return piece;
+
+    }
+
+    void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+}
